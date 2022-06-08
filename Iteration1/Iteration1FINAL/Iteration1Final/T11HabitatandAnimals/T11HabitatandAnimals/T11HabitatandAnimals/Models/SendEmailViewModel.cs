d49ad4f4f42b3ef7739d95109cb9e2925b7085c5
YanzeLi <yli518@student.monash.edu>
2022-06-09 01:59:27 +1000

    Iteration1 Delivery Version
    Kolaroo Project iteration 1 version

---
+++ Iteration1/Iteration1FINAL/Iteration1Final/T11HabitatandAnimals/T11HabitatandAnimals/T11HabitatandAnimals/Models/SendEmailViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace T11HabitatandAnimals.Models
+{
+    public class SendEmailViewModel
+    {
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string ToEmail { get; set; }
+    }
+}
